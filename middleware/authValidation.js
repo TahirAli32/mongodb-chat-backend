5f56a781caeba586273eb4a6a7def88dcf242165
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2022-10-31 15:00:18 +0500

    Auth Done

---
+++ middleware/authValidation.js
@@ -0,0 +1,24 @@
+const Joi = require('joi')
+
+// Register Validation
+const registerValidation = data => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().min(5).required(),
+    })
+    return schema.validate(data)
+}
+
+// Login Validation
+const loginValidation = data => {
+    const schema = Joi.object({
+        email: Joi.string().required().email(),
+        password: Joi.string().min(5).required(),
+        rememberMeToken: Joi.boolean()
+    })
+    return schema.validate(data)
+}
+
+module.exports.loginValidation = loginValidation
+module.exports.registerValidation = registerValidation
